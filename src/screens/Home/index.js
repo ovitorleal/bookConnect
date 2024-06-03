24cfb23b42fb51efa97f153d0a35f717e51e656c
Vitor Leal <vitorleal90@outlook.com>
2024-06-03 20:37:48 -0300

    Co-authored-by: Léo <Kiiryuu@users.noreply.github.com> Co-authored-by: Diogo Rocha <DiogoRocha22@users.noreply.github.com>

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,13 +1,27 @@
 import React from "react"
 import { livrosDisponiveis } from "../../Biblioteca/livrosDisponiveis.js";
-import { Text, View, Image, StyleSheet, SafeAreaView, ScrollView } from "react-native"
+import { Text, View, Image, StyleSheet, SafeAreaView, ScrollView } from "react-native";
+import { Container } from "../../styles/style-comum.js";
 
 export default function Home () {
     return (
         <SafeAreaView style= {styles.container}>
-            <Text>Estou na pagina home</Text>
 
+            <Image source={'/src/assets/logo.png'} style={styles.logo} />
+            {/* RECOMENDADOS */}
             <Text style= {styles.recomendados}>Recomendados</Text>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.carrossel}>
+                {livrosDisponiveis.map((livro, index) => (
+                    <View key={index} style={styles.livro}>
+                        <Image source={livro.capa} style={styles.image} />
+                        <Text>{livro.nome}</Text>
+                    </View>
+                ))}
+            </ScrollView>
+
+            {/* EM ALTA */}
+
+            <Text style= {styles.emAlta}>Em alta</Text>
             <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.carrossel}>
                 {livrosDisponiveis.map((livro, index) => (
                     <View key={index} style={styles.livro}>
@@ -23,7 +37,11 @@ export default function Home () {
 
 const styles = StyleSheet.create({
     container: {
-        marginHorizontal: 20,
+        
+        backgroundColor: 'black',
+        flex: 1,
+        
+        
     },
     carrossel: {
         marginTop: 10,
@@ -31,16 +49,36 @@ const styles = StyleSheet.create({
     livro: {
         alignItems: 'center',
         marginRight: 10,
+        
     },
     image: {
         width: 120,
         height: 150,
-        marginBottom: 10,
+        marginBottom: 5,
+    },
+    logo: {
+        width: 200,
+        height: 160,
+        resizeMode: 'contain',
+        justifyContent: 'center',
+        marginLeft: 100
     },
     recomendados: {
-        marginTop: 30,
-        marginBottom: 10,
+        marginTop: 5,
+        marginBottom: 5,
+        marginLeft: 15,
         fontSize: 18,
-        fontWeight: 'bold',
+        fontWeight: '500',
+        color: 'white',
+        fontFamily: 'Courier'
+    },
+    emAlta: {
+        marginTop: 5,
+        marginBottom: 10,
+        marginLeft: 15,
+        fontSize: 18,
+        fontWeight: '500',
+        color: 'white',
+        fontFamily: 'Courier'
     },
 });
---
+++ src/styles/style-comum.js
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+const Container  = styled.View `
+    flex: 1;
+    background-color: black;
+    align-items: center;
+    justify-content: center;
+`;
+
+
+export {
+    Container
+}
