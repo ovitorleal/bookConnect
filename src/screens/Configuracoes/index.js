7bc3418523539c8f8002d4e262005707fabcfad6
Vitor Leal <vitorleal90@outlook.com>
2024-06-19 14:59:19 -0300

    Commit 19/06/2024 - Criação tela Configurações

--- src/screens/Configuracoes/index.js
+++ src/screens/Configuracoes/index.js
@@ -1,9 +1,191 @@
 import React from "react"
 import { Text } from "react-native"
 
-export default function Configuracoes () {
 
+
+import { View, Image, StyleSheet, SafeAreaView, ScrollView, TouchableOpacity } from "react-native";
+import { CORES } from "../../constants/cores";
+
+import { SimpleLineIcons } from "@expo/vector-icons";
+
+
+
+export default function Configuracoes() {
     return (
-        <Text>Estou na pagina configurações</Text>
+        <SafeAreaView style={styles.container}>
+
+            <Image source={'/src/assets/logo.png'} style={styles.logo} />
+            <View style={styles.botoes}>
+            <TouchableOpacity style={styles.botao}>
+                {/* conteudo do botão */}
+                <View style={styles.content}>
+                    {/* usando simpleLineIcons e estilizando */}
+                    <SimpleLineIcons
+                        style={{ marginRight: 12 }}
+                        size={24}
+                        color="#fff"
+                        name="bell"
+                    />
+                    <Text style={styles.titulo}>Notificações</Text>
+                    <SimpleLineIcons
+                        style={{ marginLeft: 75 }}
+                        size={24}
+                        color="#fff"
+                        name="arrow-right"
+                    />
+
+                </View>
+
+            </TouchableOpacity>
+
+
+            <TouchableOpacity style={styles.botao}>
+                
+                <View style={styles.content}>
+                   
+                    <SimpleLineIcons
+                        style={{ marginRight: 12 }}
+                        size={24}
+                        color="#fff"
+                        name="arrow-down-circle"
+                    />
+                    <Text style={styles.titulo}>Downloads</Text>
+                    <SimpleLineIcons
+                        style={{ marginLeft: 110 }}
+                        size={24}
+                        color="#fff"
+                        name="arrow-right"
+                    />
+
+                </View>
+
+            </TouchableOpacity>
+
+
+            <TouchableOpacity style={styles.botao}>
+               
+                <View style={styles.content}>
+                   
+                    <SimpleLineIcons
+                        style={{ marginRight: 12 }}
+                        size={24}
+                        color="#fff"
+                        name="user"
+                    />
+                    <Text style={styles.titulo}>Usuário</Text>
+                    <SimpleLineIcons
+                        style={{ marginLeft: 130}}
+                        size={24}
+                        color="#fff"
+                        name="arrow-right"
+                    />
+
+                </View>
+
+            </TouchableOpacity>
+
+
+            <TouchableOpacity style={styles.botao}>
+                
+                <View style={styles.content}>
+                    
+                    <SimpleLineIcons
+                        style={{ marginRight: 12 }}
+                        size={24}
+                        color="#fff"
+                        name="earphones-alt"
+                    />
+                    <Text style={styles.titulo}>Ajuda</Text>
+                    <SimpleLineIcons
+                        style={{ marginLeft: 150 }}
+                        size={24}
+                        color="#fff"
+                        name="arrow-right"
+                    />
+
+                </View>
+
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.botao}>
+                
+                <View style={styles.content}>
+                    
+                    <SimpleLineIcons
+                        style={{ marginRight: 12 }}
+                        size={24}
+                        color="#fff"
+                        name="logout"
+                    />
+                    <Text style={styles.titulo}>Sair</Text>
+                    <SimpleLineIcons
+                        style={{ marginLeft: 160 }}
+                        size={24}
+                        color="#fff"
+                        name="arrow-right"
+                    />
+
+                </View>
+
+            </TouchableOpacity>
+            </View>
+
+
+        </SafeAreaView>
     )
+
 }
+
+const styles = StyleSheet.create({
+    container: {
+
+        backgroundColor: 'black',
+        flex: 1,
+
+
+    },
+    botoes:{
+        flex: 1,
+        marginTop: 50
+        
+    },
+    logo: {
+        width: 190,
+        height: 150,
+        resizeMode: 'contain',
+        justifyContent: 'center',
+        marginLeft: 110
+    },
+    botao: {
+        backgroundColor: CORES.COR_PRIMARIA,
+        height: 50,
+        borderRadius: 12,
+        width: '90%',
+        alignContent: "center",
+        justifyContent: "center",
+        marginHorizontal: 20,
+        marginVertical: 10
+
+    },
+
+    titulo: {
+        fontSize: 18,
+        color: 'white',
+        fontFamily: 'Courier',
+        alignContent: 'center',
+        marginLeft: 40
+
+
+    },
+    content: {
+        flexDirection: "row",
+        marginLeft: 40,
+
+
+
+
+    }
+
+
+
+});
